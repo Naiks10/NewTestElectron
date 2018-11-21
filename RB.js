077f122300dd50fb562ece19f428c6b2dd3ebec2
Unknown <Ultravirus661@gmail.com>
2018-11-21 17:30:09 +0300

    js_script for mainpage

---
+++ RB.js
@@ -0,0 +1,11 @@
+function rightbutton(button1 , button2) 
+{
+    button1.value = 'Нажата правая кнопка';
+    button2.vlaue = "Левая кнопка";   
+}
+
+function rightbutton(button1, button2) 
+{
+    button1.value = 'Нажата левая кнопка';
+    button2.vlaue = "Правая кнопка";   
+}
