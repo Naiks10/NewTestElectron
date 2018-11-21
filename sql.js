c65e01a69d113a885d7d0ca9045e8d98a4e24557
Владислав Рябов <Ultravirus661@gmail.com>
2018-11-21 19:53:37 +0300

    newElement

---
+++ sql.js
@@ -0,0 +1,26 @@
+
+function newsql(text) 
+{
+const config = {
+    user: 'sa',
+    password: '123',
+    server: 'DESKTOP-12BNNTQ\RVO', 
+    database: 'ZAK_base',
+}
+
+
+const sql = require('mssql')
+ 
+sql.connect(config).then(() => {
+    return sql.query`select * from SOVM_KOMP`
+}).then(result => {
+    console.dir(result);
+    text.value = result[0].SOV_KOMP;
+}).catch(err => {
+    // ... error checks
+})
+ 
+sql.on('error', err => {
+    // ... error handler
+})
+}
