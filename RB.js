16fb6ad595f2fc9867dc1456742a8bb93fc0ed07
Unknown <Ultravirus661@gmail.com>
2018-11-21 17:38:38 +0300

    Fix

--- RB.js
+++ RB.js
@@ -1,11 +1,11 @@
 function rightbutton(button1 , button2) 
 {
     button1.value = 'Нажата правая кнопка';
-    button2.vlaue = "Левая кнопка";   
+    button2.value = "Левая кнопка";   
 }
 
 function leftbutton(button1, button2) 
 {
     button1.value = 'Нажата левая кнопка';
-    button2.vlaue = "Правая кнопка";  
+    button2.value = "Правая кнопка";  
 }
